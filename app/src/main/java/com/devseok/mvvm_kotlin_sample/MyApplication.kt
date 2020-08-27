cde09e4ae8901219ace2f6ae2cdbf355f368af27
jinseok <seok270@gmail.com>
2020-08-27 22:34:01 +0900

    init project

---
+++ app/src/main/java/com/devseok/mvvm_kotlin_sample/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.devseok.mvvm_kotlin_sample
+
+import android.app.Application
+import org.koin.android.ext.android.startKoin
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
